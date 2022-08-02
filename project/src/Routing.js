dc54456f061c284968e1d4c4f93dad8884d90154
DipikaRane <deepikajoge@gmail.com>
2022-08-02 15:07:15 +0530

    Routing in react

---
+++ project/src/Routing.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import HomePage from './Home/HomePage';
+import Home from './Product/Home_bk';
+
+function Routing(){
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<HomePage/>}/>
+                <Route path='/product' element={<Home/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+export default Routing
--- project/src/index.js
+++ project/src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 //import App from './App';
-import Home from './Product/Home_bk';
+//import Home from './Product/Home_bk';
 import reportWebVitals from './reportWebVitals';
+import Routing from './Routing';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <Home />
+    {/* <Home /> */}
+    <Routing/>
   </React.StrictMode>
 );
 
